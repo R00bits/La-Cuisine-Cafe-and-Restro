96673e557d9dab2382740eb8365a1f2486092811
Robin Manandhar <rbynmndr@gmail.com>
2024-11-06 13:49:35 +1100

    changes

--- cuisinecafe/src/list/menu5.js
+++
@@ -1,9 +0,0 @@
-const menu5=[
-    { name: 'Milkshake', price: '$7.00', description: 'Flavours: Strawberry, Chocolate, Vanilla, Caramel' },
-    { name: 'Orange Juice', price: '$8.00', description: '' },
-    { name: 'Apple Juice', price: '$8.00', description: '' },
-    { name: 'Orange & Carrot Juice', price: '$8.00', description: '' },
-    { name: 'Apple, Beetroot, Celery & Carrot Juice', price: '$8.00', description: '' },
-    { name: 'Carrot, Lemon & Ginger Juice', price: '$8.00', description: '' },
-]
-export default menu5;
