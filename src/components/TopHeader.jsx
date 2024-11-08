4ac4a22cdb7634bf16923742a52e85c6e99fdfb9
Robin Manandhar <rbynmndr@gmail.com>
2024-11-08 20:46:44 +1100

    herosection change

---
+++ src/components/TopHeader.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../Css/topheader.css';
+
+function TopHeader(){
+    return (
+        
+        <div className="h-12 bg-primary flex justify-between items-center px-6 topheader_text">
+            <div className='flex font-medium'>
+                <span className="system-uicons--location"></span>
+                Shop 2, 118 Church Street, Parramatta NSW
+                
+            </div>
+            <div className='flex'>
+                <ul className='flex justify-between items-center space-x-4 topheader_social_link'>
+                    <li><a href='https://www.facebook.com/profile.php?id=61567603241017' target='_blank' rel='noreferrer' className='flex relative group' data-tooltip-target="bottom-tooltip"><span className="raphael--facebook"></span>
+                    <div className="absolute top-full opacity-0 invisible group-hover:opacity-100 group-hover:visible left-1/2 mt-3 -translate-x-1/2 whitespace-nowrap rounded-xl bg-[var(--secondarycolor)] py-2 px-4 text-xs text-gray-800 transition-opacity duration-300 shadow-lg z-50" role="tooltip" id="bottom-tooltip">
+<span className="absolute -top-1.5 left-1/2 -z-10 h-3 w-3 -translate-x-1/2 rotate-45 bg-[var(--secondarycolor)]"></span>
+<p className="text-xs text-[var(--primarycolor)] font-light">Follow us on facebook.</p></div>
+                    </a></li>
+                    <li><a href='https://www.instagram.com/lacuisine.cafe?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==' target='_blank' rel='noreferrer' className='flex relative group' data-tooltip-target="bottom-tooltip"><span className="simple-line-icons--social-instagram"></span>
+                    <div className="absolute top-full opacity-0 invisible group-hover:opacity-100 group-hover:visible left-1/4 mt-3 mr-1 -translate-x-1/2 whitespace-nowrap rounded-xl bg-[var(--secondarycolor)] py-2 px-4 text-xs text-gray-800 transition-opacity duration-300 shadow-lg z-50" role="tooltip" id="bottom-tooltip">
+<span className="absolute -top-1.5 left-[54%] -z-10 h-3 w-3 -translate-x-1/2 rotate-45 bg-[var(--secondarycolor)]"></span>
+<p className="text-xs text-[var(--primarycolor)] font-light">Follow us on instagram.</p></div></a></li>
+                    <li><a href='https://www.tiktok.com/@lacuisinecafe' target='_blank' rel='noreferrer' className='flex relative group' data-tooltip-target="bottom-tooltip"><span className="hugeicons--tiktok"></span>
+                    <div className="absolute top-full opacity-0 invisible group-hover:opacity-100 group-hover:visible -left-3/4 mt-3 mr-1 -translate-x-1/2 whitespace-nowrap rounded-xl bg-[var(--secondarycolor)] py-2 px-4 text-xs text-gray-800 transition-opacity duration-300 shadow-lg z-50" role="tooltip" id="bottom-tooltip">
+<span className="absolute -top-1.5 left-[74%] -z-10 h-3 w-3 -translate-x-1/2 rotate-45 bg-[var(--secondarycolor)]"></span>
+<p className="text-xs text-[var(--primarycolor)] font-light">Follow us on tiktok.</p></div></a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default TopHeader
--- src/components/herosection.jsx
+++ src/components/herosection.jsx
@@ -12,8 +12,10 @@ function HeroSection() {
     <div className="heromain">
       <div className="hero-container">
       <div className="hero-content">
-        <h1>Sip, Savor, Smile <span className="highlight">Delicious Moments</span><br></br> Every Day</h1>
+        <h1 className='text-5xl'>Sip, Savor, Smile</h1><div className="text-hover-effect"><h1 className='text-6xl'><span className="highlight">Delicious Moments</span></h1></div> <br></br> <h1 className="-mt-2 text-5xl">Every Day</h1>
         <p>Welcome to La Cuisine Cafe, where every sip and bite is crafted with love.</p>
+        
+
         {/* <div className="hero-buttons">
           <button onClick={menu} className="menu-btn">Menu</button>
           <button className="location-btn"><a href='#location'>Our Location</a></button>
